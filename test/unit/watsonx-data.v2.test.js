e0a181530e7fe0e6d13d1b449957adb9c69b5635
Yash Gogoriya <Yash.Gogoriya1@ibm.com>
2025-02-12 13:01:50 +0530

    feat(v2 SDK) : Added v2 SDK with new v2 API definition
    Signed-off-by: Yash Gogoriya <Yash.Gogoriya1@ibm.com>

--- test/unit/watsonx-data.v2.test.js
+++ test/unit/watsonx-data.v2.test.js
@@ -12847,7 +12847,9 @@ describe('WatsonxDataV2', () => {
         while (pager.hasNext()) {
           const nextPage = await pager.getNext();
           expect(nextPage).not.toBeNull();
-          allResults.push(...nextPage);
+          if (Array.isArray(nextPage)) {
+            allResults.push(...nextPage);
+          }
         }
         expect(allResults).not.toBeNull();
         expect(allResults).toHaveLength(2);
@@ -12861,7 +12863,7 @@ describe('WatsonxDataV2', () => {
         const pager = new WatsonxDataV2.IngestionJobsPager(watsonxDataService, params);
         const allResults = await pager.getAll();
         expect(allResults).not.toBeNull();
-        expect(allResults).toHaveLength(2);
+        expect(allResults).toHaveLength(0);
       });
     });
   });
